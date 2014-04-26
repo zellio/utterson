36689109bd67e5ea5aecae2ad8ecb4b86f9be28f
Zachary Elliott <zach@nyu.edu>
2014-04-26 02:03:53 -0400

    Removing useless dummy spec

--- spec/lanyon_spec.rb
+++ spec/lanyon_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
 describe Lanyon do
+
   it 'should have a version number' do
     Lanyon::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should eq(true)
-  end
 end
