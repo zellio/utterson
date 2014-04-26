cf8f12b1eff6c3d17d7638d02488e784e2fb8809
Zachary Elliott <zach@nyu.edu>
2014-04-26 01:34:55 -0400

    Adding rspec skeleton for Lanyon::File

--- spec/lanyon_file_spec.rb
+++ spec/lanyon_file_spec.rb
@@ -6,4 +6,24 @@ describe Lanyon::File do
     expect(Lanyon::File.ancestors.include? File).to be_true
   end
 
+  describe '#basename' do
+    it
+  end
+
+  describe '#dirname' do
+    it
+  end
+
+  describe '#file?' do
+    it
+  end
+
+  describe '#directory?' do
+    it
+  end
+
+  describe '#to_json' do
+    it
+  end
+
 end
