f6618eb4a04babfd15ec496d2b7e4aafe73aa4f4
Zachary Elliott <zach@nyu.edu>
2014-04-26 01:45:16 -0400

    Specing Lanyon::File#directory?

--- spec/lanyon_file_spec.rb
+++ spec/lanyon_file_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Lanyon::File do
 
   let (:file) { Lanyon::File.new path: 'path/to/foo.md' }
+  let (:dir)  { Lanyon::File.new path: 'path/to/foo/' }
 
   it 'subclasses ::File' do
     expect(Lanyon::File.ancestors.include? File).to be_true
@@ -21,11 +22,17 @@ describe Lanyon::File do
   end
 
   describe '#file?' do
-    it
+    it 'returns true if @path points to a file' do
+      expect(file.file?).to be_true
+      expect(dir.file?).to be_false
+    end
   end
 
   describe '#directory?' do
-    it
+    it 'returns true if the file is a directory' do
+      expect(file.file?).to be_false
+      expect(dir.file?).to be_true
+    end
   end
 
   describe '#to_json' do
