620c85334d49214ad91aae631e13275f8273009b
Zachary Elliott <zach@nyu.edu>
2014-04-27 00:05:12 -0400

    Setting up sinatra paths

--- lib/lanyon/application.rb
+++ lib/lanyon/application.rb
@@ -1,4 +1,7 @@
 require 'sinatra/base'
 
 class Lanyon::Application < Sinatra::Base
+  set :root, -> { __dir__ }
+  set :views, -> { File.join(root, 'templates') }
+  set :public_folder, -> { File.join(root, 'assets') }
 end
