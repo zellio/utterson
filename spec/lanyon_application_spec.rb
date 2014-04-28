2d2a2726355a1b8c2c37874efa9f93375b51f66f
Zachary Elliott <zach@nyu.edu>
2014-04-27 20:38:54 -0400

    Adding basic Lanyon::Application tests

--- spec/lanyon_application_spec.rb
+++ spec/lanyon_application_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 
 describe Lanyon::Application do
 
-  it
+  let(:app) { Lanyon::Application.new }
+
+  it 'loads views from "templates"' do
+    expect(File.basename(app.settings.views)).to eql 'templates'
+  end
+
+  it 'loads public content from "assets"' do
+    expect(File.basename(app.settings.public_folder)).to eql 'assets'
+  end
 
 end
