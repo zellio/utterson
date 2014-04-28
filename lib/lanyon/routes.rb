160a438fc30bb18a149bf081d5821f23b818785c
Zachary Elliott <zach@nyu.edu>
2014-04-27 20:56:30 -0400

    Adding route loading code

--- lib/lanyon/routes.rb
+++ lib/lanyon/routes.rb
@@ -1,2 +1,16 @@
-module Lanyon::Routes
+module Lanyon
+  module Route; end
+
+  module Routes
+    Dir[File.join(File.dirname(__FILE__), 'routes', '*.rb')].each do |file|
+      require file
+    end
+
+    def self.registered(app)
+      Route.constants.each do |const|
+        _module = Lanyon::Route.const_get const
+        app.reqister(_module) if _module.class == Module
+      end
+    end
+  end
 end
