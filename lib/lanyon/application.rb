1a052dc69a410e2d51037c1e72e0e32672c6684a
Zachary Elliott <zach@nyu.edu>
2014-04-28 23:40:39 -0400

    Setting up liquid partials

--- lib/lanyon/application.rb
+++ lib/lanyon/application.rb
@@ -1,10 +1,14 @@
 require 'sinatra/base'
 require 'sinatra/config_file'
+require 'liquid'
 
 class Lanyon::Application < Sinatra::Base
   set :root, -> { __dir__ }
   set :views, -> { File.join(root, 'templates') }
   set :public_folder, -> { File.join(root, 'assets') }
+
+  Liquid::Template.file_system = Liquid::LocalFileSystem.new(views)
+  set :liquid, :layout => :base
 
   register Sinatra::ConfigFile
   config_file 'config.yml'
