2b45a750617dd72871aa1c50635cb6713d228b5a
Zachary Elliott <zach@nyu.edu>
2014-05-01 00:15:20 -0400

    Making this obviously a CRUD app

--- lib/lanyon/route/files.rb
+++ lib/lanyon/route/files.rb
@@ -4,8 +4,24 @@ module Lanyon::Route::Files
   def self.registered(app)
     repo = Rugged::Repository.new(app.repo_dir)
 
-    app.get '/files/*', provides: [:html, :json] do
+    # CREATE
+    app.post '/files' do
+    end
+
+    # READ
+    app.get '/files/*/?:id?', provides: [:html, :json] do
+      path = "./#{params[:splat].first}"
+      oid = params[:id]
+
       respond_with :files, files: nil
+    end
+
+    # UPDATE
+    app.put '/files' do
+    end
+
+    # DELETE
+    app.delete '/files' do
     end
   end
 end
