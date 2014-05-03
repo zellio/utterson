2dd68b311e91745f056134e2d6e14a506c271f04
Zachary Elliott <zach@nyu.edu>
2014-05-03 14:49:34 -0400

    Improving file_collection methods to ls and ls_r

--- lib/lanyon/file_collection.rb
+++ lib/lanyon/file_collection.rb
@@ -3,7 +3,19 @@ class Lanyon::FileCollection < ::Array
     super index.map { |data| Lanyon::File.new(data[:path], data[:oid]) }
   end
 
-  def in(path)
-    []
+  def files_in(path)
+    select {|f| f.dirname == path }
+  end
+
+  def directories_in(path)
+    map {|f| f.dirname[%r{(#{path}/[^/]+)}, 1] }.compact.uniq
+  end
+
+  def ls(path)
+    directories_in(path) + files_in(path)
+  end
+
+  def ls_r(path)
+    select {|f| f.dirname.index(path) == 0 }
   end
 end
