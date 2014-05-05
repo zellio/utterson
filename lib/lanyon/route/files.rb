ca6112d66a5decc586f3160d5756e8ecb745d534
Zachary Elliott <zach@nyu.edu>
2014-05-05 16:55:46 -0400

    First pass at the UPDATE method for files route

--- lib/lanyon/route/files.rb
+++ lib/lanyon/route/files.rb
@@ -5,6 +5,12 @@ module Lanyon::Route::Files
     repo = Rugged::Repository.new(app.repo_dir)
     fc = Lanyon::FileCollection.new(repo.index)
 
+    author = {
+      email: 'lanyon@localhost.localhost',
+      name: 'Dr. Lanyon',
+      time: Time.now
+    }
+
     # CREATE
     app.post '/files/?' do
     end
@@ -21,6 +27,45 @@ module Lanyon::Route::Files
 
     # UPDATE
     app.put '/files/?' do
+      oid = request.params[:oid]
+      path = request.params[:path]
+      basename = request.params[:basename]
+      dirname = request.params[:dirname]
+      contents = request.params[:contents]
+
+      file = fc.get(oid)
+
+      halt 501 if oid.nil? || file.nil?
+
+      if path.nil?
+        path = if dirname && basename
+                 File.join(dirname, basename)
+               elsif dirname && basename.nil?
+                 File.join(dirname, file.basename)
+               elsif dirname.nil? && basename
+                 File.join(file.dirname, basename)
+               end
+      end
+
+      if file.path != path
+        # move the file
+      end
+
+      if file.contents != contents
+        tree = repo.index.write_tree
+
+        file.contents = contents
+        repo.index.add(file.path)
+
+        Rugged::Commit.create(repo, {
+          tree: tree,
+          author: author,
+          committer: author,
+          message: "Dr. Lanyon is updating <#{file.path}> with new content",
+          parents: repo.empty? ? [] : [repo.head.target].compact,
+          update_ref: 'HEAD'
+        })
+      end
     end
 
     # DELETE
