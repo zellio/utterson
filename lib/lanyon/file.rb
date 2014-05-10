ae18511962270cf811a57e76c137a03e9f578aa3
Zachary Elliott <zach@nyu.edu>
2014-05-09 20:18:35 -0400

    Making the file class more useful / stateful

--- lib/lanyon/file.rb
+++ lib/lanyon/file.rb
@@ -1,44 +1,56 @@
 require 'pathname'
 
 class Lanyon::File
-  attr_accessor :path
+  attr_accessor :path, :content
   attr_reader :oid
 
-  def initialize(path, oid)
-    @path = Pathname.new(path).absolute? ? path : File.join('.', path)
+  def initialize(root, path, oid, content = nil)
+    @root = root
+    @path = path
     @oid = oid
+    @content = content || read
+  end
+
+  def web_path
+    File.join('.', @path)
+  end
+
+  def full_path
+    File.join(@root, @path)
   end
 
   def basename
-    File.basename(@path)
+    File.basename(web_path)
   end
 
   def dirname
-    File.dirname(@path)
+    File.dirname(web_path)
   end
 
   def file?
-    File.file?(@path)
+    File.file?(full_path)
   end
 
   def directory?
-    File.directory?(@path)
+    File.directory?(full_path)
   end
 
-  def contents
-    @contents ||= File.read(@path)
+  def read
+    File.read(full_path) if file?
   end
 
-  def contents=(content)
-    File.open(@path, 'w') { |f| f.write(content) }
-    @contentes = content
+  def write
+    File.open(full_path, 'w') { |f| f.write(content) } if file?
+  end
+
+  def move(path)
+    File.rename(full_path, File.join(@root, path))
+    @path = path
   end
 
   def to_json(state)
     { oid: @oid,
       path: @path,
-      basename: basename,
-      dirname: dirname,
-      contents: contents }.to_json
+      content: content }.to_json
   end
 end
