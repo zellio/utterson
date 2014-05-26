7da37d6011eeca2435b3738baaec4ce68c63289a
Zachary Elliott <zach@nyu.edu>
2014-05-25 22:32:33 -0400

    Removing unneeded require

--- lib/lanyon.rb
+++ lib/lanyon.rb
@@ -3,7 +3,6 @@ module Lanyon
   require 'lanyon/file_object'
   require 'lanyon/file'
   require 'lanyon/directory'
-  require 'lanyon/file_collection'
   require 'lanyon/repository_manager'
   require 'lanyon/routes'
   require 'lanyon/application'
