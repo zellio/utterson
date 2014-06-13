7edf02a115f6c3939a8b2dda25e07ffd18144a69
Zachary Elliott <zach@nyu.edu>
2014-06-13 16:58:14 -0400

    Adding specs for Lanyon::{File,Directory}

--- spec/lanyon_repository_manager_spec.rb
+++ spec/lanyon_repository_manager_spec.rb
@@ -5,6 +5,9 @@ describe Lanyon::RepositoryManager  do
   let(:repo_dir) { File.join(__dir__, "spec_repo.#{Time.now.to_i}") }
   let(:repo_manager) { Lanyon::RepositoryManager.new(repo_dir) }
 
+  let(:foid) { '9eec82ced116713a102a04307e7e18d2509f8698' }
+  let(:doid) { '591c274b49158e1a7dfcc962c190e3f347ee37dd' }
+
   around(:each) { |example| fakegit(repo_dir, &example) }
 
   describe '#initialize' do
@@ -27,10 +30,35 @@ describe Lanyon::RepositoryManager  do
   end
 
   describe '#file' do
-    it
+    let(:lfile) { repo_manager.file('README.md') }
+
+    it 'Returns a Lanyon::File' do
+      expect(lfile).to be_a Lanyon::File
+    end
+
+    it 'Looks up a file by name' do
+      expect(lfile.oid).to eql foid
+    end
+
+    it 'Returns nil if the file doesn\'t exist' do
+      expect(repo_manager.file('fake_file')).to be_nil
+    end
   end
 
   describe '#directory' do
+    let(:ldir) { repo_manager.directory('src') }
+
+    it 'Returns a Lanyon::Directory' do
+      expect(ldir).to be_a Lanyon::Directory
+    end
+
+    it 'Looks up a directory by name' do
+      expect(ldir.oid).to eql doid
+    end
+
+    it 'Returns nil if the directory doesn\'t exist' do
+      expect(repo_manager.file('fake_dir')).to be_nil
+    end
   end
 
   describe '#add' do
