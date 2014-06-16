e73d726a1beb0bcb734d9f6439994a232e2b83e4
Zachary Elliott <zach@nyu.edu>
2014-06-16 00:12:07 -0400

    Starting on file spec

--- spec/lanyon_route_files_spec.rb
+++ spec/lanyon_route_files_spec.rb
@@ -1,12 +1,45 @@
 require 'spec_helper'
 
-describe Lanyon::Route::Files do
+describe Lanyon::Route::Files, rackup: true do
+
+  let(:repo_dir) { File.join(__dir__, "spec_repo.#{Time.now.to_i}") }
+  # let(:repo_manager) { Lanyon::RepositoryManager.new(repo_dir) }
+
+  let(:foid) { '9eec82ced116713a102a04307e7e18d2509f8698' }
+
+  around(:each) { |example| fakegit(repo_dir, &example) }
+
   describe 'POST' do
     it
   end
 
   describe 'GET' do
-    it
+    it '/files' do
+      post '/files'
+      expect(last_response).to be_ok
+    end
+
+    it '/files/' do
+      get '/files/'
+      expect(last_response).to be_ok
+    end
+
+    describe '/files/:path' do
+      it 'returns the file content as JSON' do
+        get '/files/README.md', nil, { 'HTTP_ACCEPT' => 'application/json' }
+
+        val = JSON.parse(last_response.body)
+
+        expect(last_response).to be_ok
+        expect(val['file']).to be_truthy
+        expect(val['file']['oid']).to eql foid
+      end
+
+      it '404s on a bad path' do
+        get '/files/bad_path'
+        expect(last_response).to be_not_found
+      end
+    end
   end
 
   describe 'UPDATE' do
